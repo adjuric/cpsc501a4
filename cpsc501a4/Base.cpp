746af93a7c4fbf05afd5d8dd58db0bafdc23c0e8
Aleks <Aleks@Aleks-PC>
2016-12-08 14:44:52 -0700

    Adding the Convolution method from lecture

--- cpsc501a4/Base.cpp
+++ cpsc501a4/Base.cpp
@@ -1,2 +1,30 @@
 /*	Aleksandar Djuric
- 	*/
+	10057804
+ */
+ 
+ #include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <fstream>
+#include <stdlib.h>
+#include <malloc.h>
+#include <time.h>
+#include "Base.h"
+
+using namespace std;
+
+// Code from Lecture
+void baseConvolution(float x[], int N, float h[], int M, float y[], int P) {
+	int n, m;
+
+	for (n = 0; n < P; n++)
+		y[n] = 0.0;
+
+	float percent = 0.0;
+	for (n = 0; n < N; n++) {
+		for (m = 0; m < M; m++)
+		{
+			y[n+m] += x[n] * h[m];
+		}
+	}
+}
--- cpsc501a4/Base.h
+++ cpsc501a4/Base.h
@@ -1,3 +1,5 @@
 #ifndef BASE_H_
 #define BASE_H_
+
+void baseConvolution(float x[], int N, float h[], int M, float y[], int P);
 #endif /* BASE_H_ */

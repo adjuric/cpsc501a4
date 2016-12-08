691fc9f9f83345f19f80427ce7cdc34c19b8a2ca
Aleks <Aleks@Aleks-PC>
2016-12-08 15:44:29 -0700

    Adding a function for writing the File (Derived from tutorial Example)

--- cpsc501a4/Base.h
+++ cpsc501a4/Base.h
@@ -1,5 +1,7 @@
 #ifndef BASE_H_
 #define BASE_H_
-
+float* readFile(char *fileName, float *signal, int *Thesize);
+void writeFile(char *fileName, int numSamples, float *signal);
 void baseConvolution(float x[], int N, float h[], int M, float y[], int P);
+void scaleWave(float signal[], int samples);
 #endif /* BASE_H_ */

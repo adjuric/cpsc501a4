c9fdd94e8e8ed906e46deb1739dfd8d423eeb900
Aleks <Aleks@Aleks-PC>
2016-12-08 19:18:13 -0700

    Functions Applied in Main

--- cpsc501a4/ABased.cpp
+++ cpsc501a4/ABased.cpp
@@ -2,7 +2,6 @@
 	10057804
  */
 
-#include "ABased.h"
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,12 +9,86 @@
 #include "Base.h"
 #include <math.h>
 #include <time.h>
+#include "ABased.h"
 
 int fsize;
 clock_t t1, t2;
 #define PI	3.141592653589793
 #define TWO_PI	(2.0 * PI)
 #define SWAP(a,b)	tempr=(a);(a)=(b);(b)=tempr
+
+void abased(char *inputFile, char *IRfile, char *outputfile){
+
+	printf("Frequency Based\n");
+	char *DSound = inputFile;
+	char *IResponse = IRfile;
+	char *convolution = outputfile;
+	int inputFileSignal;
+	int IRFileSignal;
+
+	// Data from both Dry and Impulse
+	float* inputFileSignal = readData(DSound, inputFileSignal, &fsize);
+	float* IRFileSignal = readData(IResponse, IRFileSignal, &fsize);
+	inputFileSignal = fsize;
+	IRFileSignal = fsize;
+
+	// Intiating Array and Setting Size opf the output
+	int outputFileSignal = inputFileSignal + IRFileSignal - 1;
+	float *outputFileSignal = new float[outputFileSignal];
+
+	int padSize = inputFileSignal + IRFileSignal - 1;
+	int totalsize = 1;
+	int someNum = 0;
+	while(totalsize < padSize){
+		totalsize <<= someNum;
+		someNum++;
+	}
+
+	// Creating the Padding for the Dry File
+	float *complex = new float [2*totalsize];
+	float *input = new float[2*totalsize];
+	float *ir = new float [2*totalsize];
+
+	int i, k;
+	for(i = 0, k = 0; i < inputFileSignal; i++, k+=2){
+	    input[k] = inputFileSignal[i];
+	    input[k + 1] = 0;
+	}
+
+	// pad for the impulse response file
+	i = k;
+	memset(input + k, 0, (2*totalsize) -1);
+
+	for(i = 0, k = 0; i < IRFileSignal; i++, k+=2){
+	    ir[k] = IRFileSignal[i];
+	    ir[k + 1] = 0;
+	}
+	i = k;
+	memset(ir + k, 0, (2*totalsize) -1);
+	memset(complex, 0, (2*totalsize));
+	
+	//Using the FFT Algorithm from class with Padded Results
+	fft(input-1, totalsize, 1);
+	fft(ir-1, totalsize, 1);
+
+	//Solving the Complex Number
+	solve(input, ir, complex, totalsize);
+	fft(complex-1, totalsize,-1);
+
+	for(i=0; i < totalsize; i++){
+		int sqrd = i*2;
+		complex[sqrd] /= (float)totalsize;
+		complex[(sqrd)+1] /= (float)totalsize;
+	}
+
+	for(i=0, k=0; i < outputFileSignal; i++, k+=2){
+		outputFileSignal[i]=complex[k];
+	}
+
+	//Reusuing the Write and Scale functions in the Base Program
+	scaleWave(outputFileSignal,outputFileSignal);
+	writeFile(convolution, outputFileSignal, outputFileSignal);
+}
 
 // Created with the class material
 void fft(float data[], int nn, int isign){
--- cpsc501a4/ABased.h
+++ cpsc501a4/ABased.h
@@ -1,5 +1,6 @@
 #ifndef ABASED_H_
 #define ABASED_H_
+void base(char *inputFile,char *IRfile, char *outputfile);
 void solve(float wavInput[], float irInput[], float result[], int size);
 void fft(float data[], int nn, int isign);
 #endif /* ABASED_H_ *
